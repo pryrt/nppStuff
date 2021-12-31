b940c8cc8a82cbdc8e1c926e768a9ee274f3cdbb
PeterCJ <pryrtcode@pryrt.com>
2021-12-31 11:50:57 -0800

    function list tester is somewhat working; enough to remove dev- prefix

---
+++ pythonScripts/functionListTester.py
@@ -0,0 +1,149 @@
+"""functionListTester
+
+author = PeterJones @ community.notepad-plus-plus.org
+
+This script will [hopefully] load a functionList XML file, extract its regexes, and try to indicate what will and won't match (to prevent needing to restart Notepad++ while doing development on a functionList)
+
+1. Open the functionList XML in editor2
+2. Open the file with the function names in editor1
+
+derived from my sessionChecker.py
+
+"""
+
+# reference: https://stackoverflow.com/questions/48746478/how-do-i-extract-value-of-xml-attribute-in-python
+# reference: https://docs.python.org/2/library/xml.etree.elementtree.html
+import xml.etree.ElementTree as ET
+import os.path
+import re
+
+# I originally hoped I could make a modal MessageBox, so that could prompt the user to move files around to the right place
+# r = notepad.messageBox("message", "title", MESSAGEBOXFLAGS.OKCANCEL);
+# replace that with the assumption of editor2==XML, editor1==FileWithFunctions
+
+xmlEditor = editor2
+funcEditor = editor1
+
+try:
+    console.write("try xml=editor2\n")
+    xmlTree = ET.fromstring(xmlEditor.getText())
+except ET.ParseError:
+    # swap if not right
+    xmlEditor = editor1
+    funcEditor = editor2
+    try:
+        console.write("try xml=editor1\n")
+        xmlTree = ET.fromstring(xmlEditor.getText())
+    except ET.ParseError:
+        raise ValueError('Neither editor is showing an XML file') # TODO: add buffer filenames
+
+console.write("xmlEditor = {} in editor{}\n".format(xmlEditor, "2" if xmlEditor==editor2 else "1"))
+console.write("funcEditor = {} in editor{}\n".format(funcEditor, "1" if funcEditor==editor1 else "2"))
+
+if xmlTree.find('./functionList') is None:
+    raise ValueError('XML did not have functionList', ET.tostring(xmlTree))
+
+parser = xmlTree.find('./functionList/parser')
+
+#console.write("whole XML:\n==========\n{}\n==========\n".format(ET.tostring(xmlTree)))
+
+# these don't exist by default, so add them to an inheriting enumeration
+class MY_MENUCOMMAND(MENUCOMMAND):
+    SEARCH_UNMARKALLEXT = 43032
+    SEARCH_MARKONEEXT1 = 43062
+    SEARCH_MARKONEEXT2 = 43063
+    SEARCH_MARKONEEXT3 = 43064
+    SEARCH_MARKONEEXT4 = 43065
+    SEARCH_MARKONEEXT5 = 43066
+
+    def unused():
+        pass
+
+#console.write("inherit.SEARCH_UNMARKALLEXT1={:d}, PCJ={:d}\n".format(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT1, MY_MENUCOMMAND.PCJ_SEARCH_UNMARK_ALL))
+
+def markingFunction(m,ext):
+    #console.write("match m={} span={}\n".format(m.group(0), m.span(0)))
+    mstart, mend = m.span(0)
+    #mlen = mend - mstart
+    #funcEditor.startStyling(mstart,0)
+    #funcEditor.setStyling(mlen, 14)
+    funcEditor.setSelection(mstart, mend)
+    notepad.menuCommand(ext)
+    funcEditor.setSelection(0,0)
+    return
+
+def containerMarkingFunction(m,re2,ext):
+    mstart, mend = m.span(0)
+    funcEditor.research(re2, lambda m: markingFunction(m,ext), re.S|re.M, mstart, mend, 1)
+    return
+
+#### First, clear all EXT marks (token styles)
+notepad.menuCommand(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT)
+
+#### COMMENTS are EXT1
+
+try:
+    reComment = parser.attrib['commentExpr']
+    funcEditor.research(reComment, lambda m: markingFunction(m,MY_MENUCOMMAND.SEARCH_MARKONEEXT1))
+except KeyError:
+    notepad.menuCommand(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT1)
+
+#console.write("reComment = {}\n".format(reComment))
+
+#### CLASS RANGE are EXT5 (if it doesn't find CLASS NAME in the CLASS RANGE)
+#### CLASS NAMES are EXT2
+#### FUNCTION NAMES in CLASS are EXT3
+
+try:
+    classRange = parser.find('./classRange')
+    reClassMain = classRange.attrib['mainExpr']
+    console.write('<classRange MainExpr="{}">\n'.format(reClassMain))
+    #funcEditor.research(reClassMain, lambda m: markingFunction(m,MY_MENUCOMMAND.SEARCH_MARKONEEXT5), re.S|re.M)
+    classNameExpressions = classRange.findall('./className/nameExpr')
+    if len(classNameExpressions) == 0:
+        console.write("\tno class name expressions\n")
+        pass
+    else:
+        console.write("\tfound {:d} class name expressions\n".format(len(classNameExpressions)))
+        for classElem in classNameExpressions:
+            reClassName = classElem.attrib['expr']
+            console.write("\t<nameExpr expr='{}'>\n".format(reClassName))
+            funcEditor.research(reClassMain, lambda m: containerMarkingFunction(m,reClassName,MY_MENUCOMMAND.SEARCH_MARKONEEXT2), re.S|re.M)
+    classFunction = classRange.find('./function')
+    reClassFunctionMain = classFunction.attrib['mainExpr']
+    console.write("\t<classRange><function mainExpr='{}'>\n".format(reClassFunctionMain))
+    classFunctionExpressions = classFunction.findall('./functionName/funcNameExpr')
+    if len(classFunctionExpressions) == 0:
+        funcEditor.research(reClassMain, lambda m: containerMarkingFunction(m,reClassFunctionMain,MY_MENUCOMMAND.SEARCH_MARKONEEXT3), re.S|re.M)
+    else:
+        for funcNameElem in classFunctionExpressions:
+            reClassFuncName = funcNameElem.attrib['expr']
+            funcEditor.research(reClassFunctionMain, lambda m: containerMarkingFunction(m,reClassFuncName,MY_MENUCOMMAND.SEARCH_MARKONEEXT3), re.S|re.M)
+
+except KeyError:
+    notepad.menuCommand(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT2)
+    notepad.menuCommand(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT3)
+    notepad.menuCommand(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT5)
+
+#### NORMAL FUNCTION NAMES are EXT4
+
+try:
+    functionRange = parser.find('./function')
+    reFunctionMain = functionRange.attrib['mainExpr']
+    nameExpressions = functionRange.findall('./functionName/nameExpr')
+    console.write('<function MainExpr="{}"> => {:d}\n'.format(reFunctionMain, len(nameExpressions)))
+    if len(nameExpressions) == 0:
+        funcEditor.research(reFunctionMain, lambda m: markingFunction(m,MY_MENUCOMMAND.SEARCH_MARKONEEXT4), re.S|re.M)
+    else:
+        for nameElem in nameExpressions:
+            reFunctionName = nameElem.attrib['expr']
+            console.write("\t<nameExpr expr='{}'>\n".format(reFunctionName))
+            funcEditor.research(reFunctionMain, lambda m: containerMarkingFunction(m,reFunctionName,MY_MENUCOMMAND.SEARCH_MARKONEEXT4), re.S|re.M)
+    # TODO: implement className/nameExpr inside the normal function as well, using EXT2
+except KeyError:
+    notepad.menuCommand(MY_MENUCOMMAND.SEARCH_UNMARKALLEXT5)
+
+
+'''
+triple quotes
+'''
