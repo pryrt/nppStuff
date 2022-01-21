04bc102148a2e309d189bf6884928feb5e9242f9
PeterCJ <pryrtcode@pryrt.com>
2022-01-21 11:26:51 -0800

    show how to call Alan's mouse-wiggle script at startup

--- pythonScripts/startup.py
+++ pythonScripts/startup.py
@@ -35,4 +35,8 @@ editor.setSelEOLFilled(True)
 import UriIndicatorAltClick
 uiac = UriIndicatorAltClick.UIAC()
 
+# see https://community.notepad-plus-plus.org/post/71772
+#import MouseJiggleDaemon
+#mjd = MouseJiggleDaemon.MJD()
+
 console.write("END of user startup.py\n\n")
