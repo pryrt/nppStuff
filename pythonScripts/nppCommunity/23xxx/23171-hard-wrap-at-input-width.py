565edba0cdc38fc7d6727ddc9e8fd901e4460af8
PeterCJ <pryrtcode@pryrt.com>
2022-06-27 09:50:03 -0700

    added comment when I published it

--- pythonScripts/nppCommunity/23xxx/23171-hard-wrap-at-input-width.py
+++ pythonScripts/nppCommunity/23xxx/23171-hard-wrap-at-input-width.py
@@ -5,6 +5,6 @@ This will prompt for a number, and then run a regex to hardwrap after that colum
 """
 from Npp import editor,notepad,console
 
-colstr = notepad.prompt("Column", "Hard Wrap at Column", 80)
+colstr = notepad.prompt("Column", "Hard Wrap at Column", 80) # defaults to 80 characters, but you can just type over the value in the prompt box
 srch = r'^.{' + colstr + '}'
 editor.rereplace(srch, r'$0\r\n')
