bcdfd595cd590dc9991f5f44dadd1a6f080770c2
PeterCJ <pryrtcode@pryrt.com>
2023-01-06 09:40:27 -0800

    irony: using PerlScript to launch the active PythonScript file in the PythonScript plugin
    This simple Win32::Mechanize::NotepadPlusPlus script was able to accomplish what PythonScript has never been able to accomplish natively: looking up the name of the active script, and if it's found in the Python Script > Scripts menu hierarchy, run that menu command to launch that script.  This will make debugging so much easier: I don't have to find it the first time then use Ctrl+Shift+F12 to re-run most-recent script! :-)

---
+++ pythonScripts/runCurrentPythonScript.pl
@@ -0,0 +1,18 @@
+#!perl
+
+use 5.012; # strict, //
+use warnings;
+
+use Win32::Mechanize::NotepadPlusPlus ':main';
+
+for(notepad->getCurrentFilename()) {
+    m{PythonScript[\\/]scripts[\\/](.*)[\\/]}i;
+    my ($tail) = $1 // '';
+    s{.*[\\/]}{}g;
+    s{\.py$}{}g;
+    my @menuNames = ('Plugins', 'Python Script', 'Scripts', split(m{[\\/]}, $tail), $_);
+    my $status = notepad->runMenuCommand(@menuNames) || 0;
+    print "runMenuCommand(", join(' | ', @menuNames), ") => $status\n";
+}
+
+# Run entry should be `perl "C:\Users\PJones2\AppData\Roaming\Notepad++\plugins\config\PythonScript\scripts\runCurrentPythonScript.pl"`
