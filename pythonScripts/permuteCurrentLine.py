77202da6bd70c4073b6574148ae176805f0f6d9b
PeterCJ <pryrtcode@pryrt.com>
2023-02-06 11:38:57 -0800

    add a usage example

--- pythonScripts/permuteCurrentLine.py
+++ pythonScripts/permuteCurrentLine.py
@@ -1,5 +1,19 @@
 # encoding=utf-8
-"""Permutes the non-EOL characters of the current line, and duplicates it to the lines below"""
+"""Permutes the non-EOL characters of the current line, and duplicates it to the lines below
+
+Usage example:
+- I know there are letters NTH somewhere, and letter I in the second position, and there is one unknown char left
+  so I want to permute the non-I characters (including unknowns), then insert the I, then try things out:
+  1. Input nth_ as the only line in a file: use lowercase, so that DSpellCheck will work later
+  2. PythonScript > scripts > permuteCurrentLine => there will now be 4!=24 lines (in a different example, might be 5!=120)
+  3. insert i as the second character in all the lines
+  4. if there are letters that I know cannot go in certain slots, remove those
+  5. There will likely be less than a dozen letters left as possibilities, so for each letter (including the ones that are already used elsewhere, in case of duplicates)
+     a. REPLACE( _ => x ), where x is the letter
+     b. look for spellcheck results -- save any that are possible
+     c. UNDO
+     d. that should give a smaller list of words -- maybe even just one word
+"""
 from Npp import editor,notepad,console
 from itertools import permutations
 
