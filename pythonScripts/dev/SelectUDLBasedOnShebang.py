65e0ffb988e5747dbfba5954174f356c07b17c60
PeterCJ <pryrtcode@pryrt.com>
2023-09-16 16:53:39 -0700

    starting into the callbacks
    register callback
    
    grab bufferID's file extension
    
    TODO: if known extension, look for shebang

--- pythonScripts/dev/SelectUDLBasedOnShebang.py
+++ pythonScripts/dev/SelectUDLBasedOnShebang.py
@@ -17,9 +17,10 @@ CONFIGURE:
     - the NameOfUDL must match the name of your UDL exactly
 2. save
 """
-from Npp import editor,notepad,console
+from Npp import editor,notepad,console,NOTIFICATION
+import os
 
-class ThisIsTheClass(object):
+class SelectUdlBasedOnShebang(object):
     CONFIG = {
         ".mscript" : {                      # the extension, including the .
             "#UDL!mscript1": "mscript1",    # firstLineText : NameOfUDL
@@ -30,13 +31,52 @@ class ThisIsTheClass(object):
             "#UDL!ext2": "ext2",            # firstLineText : NameOfUDL
         },
     }
-    def go(self):
-        #console.write("pass")
-        pass
+    def __init__(self):
+        '''Initialize the new instance'''
+        current_version = notepad.getPluginVersion()
+        if current_version < '2.0.0.0':
+            notepad.messageBox('It is needed to run PythonScript version 2.0.0.0 or higher',
+                               'Unsupported PythonScript verion: {}'.format(current_version))
+            return
 
-ThisIsTheClass().go()
-console.show()
-console.write(str(ThisIsTheClass.CONFIG)+"\n")
+        # delete from final version: for debug, I want it to clear callbacks before registering
+        notepad.clearCallbacks()
+
+        # setup callbacks
+        notepad.callback(self.on_bufferactivated, [NOTIFICATION.BUFFERACTIVATED])
+        console.write("Registered on_bufferactivated callback for SelectUdlBasedOnShebang\n" )
+
+    def on_bufferactivated(self, args):
+        '''
+            This callback called every time document is switched.
+            Triggers the check if the document is of interest.
+
+            Args: provided by notepad object; the args of interest:
+                args['bufferID']:   ID for the activated buffer
+
+        '''
+        console.write("on_bufferactivated: "+str(args)+"\n")
+        self.check_for_udl(args['bufferID'])
+
+    def check_for_udl(self, bufferID):
+        '''
+            Check if the active buffer has a known extension for shebang processing
+            If so, trigger the UDL-change
+
+            Args:
+                bufferID:   ID for the buffer of interest
+        '''
+        filename = notepad.getBufferFilename(bufferID)
+        console.write("checking active buffer {}:{} for UDL\n".format(bufferID,filename))
+        (_,ext) = os.path.splitext(filename)
+        ext = ext.strip('.')
+        console.write("\text = '{}'\n".format(ext))
+
+    def main(self):
+        console.write("pass\n")
+
+selectUdlBasedOnShebang = SelectUdlBasedOnShebang()
+selectUdlBasedOnShebang.main()
 
 """
 Development Notes:
