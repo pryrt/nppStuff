fb7f1fe921cad6d2f1f2d953657d5ab082fa3243
PeterCJ <pryrtcode@pryrt.com>
2023-09-17 15:20:26 -0700

    Finish initial development on SelectUDLBasedOnShebang

--- pythonScripts/dev/SelectUDLBasedOnShebang.py
+++ pythonScripts/dev/SelectUDLBasedOnShebang.py
@@ -7,15 +7,24 @@ INSTALLATION
 1. Follow the instructions https://community.notepad-plus-plus.org/topic/23039/faq-desk-how-to-install-and-run-a-script-in-pythonscript
     to install PythonScript Plugin and save this script as `SelectUDLBasedOnShebang.py`
 2. You will want to follow the "Starutp script" instructions in that FAQ as well, with the following two lines
-TBD
-TBD
+
+import SelectUDLBasedOnShebang
+selectUDLBasedOnShebang = SelectUDLBasedOnShebang.SelectUDLBasedOnShebang()
+
 
 CONFIGURE:
-1. you will need to go to the line with "CONFIG ="
+Go to the line with "CONFIG =" and edit the contents
     - the extension should include the dot in the quotes
     - the firstLineText is the text that should match the "shebang" line (the first line of your file)
     - the NameOfUDL must match the name of your UDL exactly
-2. save
+Save
+Run the script (Plugins > Python Script > Scripts > SelectUDLBasedOnShebang) or restart Notepad++
+
+From now on (including after restart), anytime you activate the buffer of a file
+ending with a known extension (one of the extensions in CONFIG), it will
+look at the first line, and if the first line exactly matches one of the
+firstLineText strings in the CONFIG table, then it will activate the UDL
+that has exactly the NameOfUdl
 """
 from Npp import editor,notepad,console,NOTIFICATION
 import os
@@ -39,12 +48,13 @@ class SelectUdlBasedOnShebang(object):
                                'Unsupported PythonScript verion: {}'.format(current_version))
             return
 
-        # delete from final version: for debug, I want it to clear callbacks before registering
-        notepad.clearCallbacks()
-
         # setup callbacks
         notepad.callback(self.on_bufferactivated, [NOTIFICATION.BUFFERACTIVATED])
         console.write("Registered on_bufferactivated callback for SelectUdlBasedOnShebang\n" )
+
+        # run the initial check
+        self.check_for_udl(notepad.getCurrentBufferID())
+
 
     def on_bufferactivated(self, args):
         '''
@@ -53,9 +63,10 @@ class SelectUdlBasedOnShebang(object):
 
             Args: provided by notepad object; the args of interest:
                 args['bufferID']:   ID for the activated buffer
+            Return:
+                Nothing
 
         '''
-        console.write("on_bufferactivated: "+str(args)+"\n")
         self.check_for_udl(args['bufferID'])
 
     def check_for_udl(self, bufferID):
@@ -65,18 +76,32 @@ class SelectUdlBasedOnShebang(object):
 
             Args:
                 bufferID:   ID for the buffer of interest
+            Return:
+                Nothing
         '''
         filename = notepad.getBufferFilename(bufferID)
-        console.write("checking active buffer {}:{} for UDL\n".format(bufferID,filename))
         (_,ext) = os.path.splitext(filename)
-        ext = ext.strip('.')
-        console.write("\text = '{}'\n".format(ext))
+        if ext in self.CONFIG:
+            self.check_shebang( self.CONFIG[ext] )
 
-    def main(self):
-        console.write("pass\n")
+    def check_shebang(self, shebangs):
+        '''
+            Check if the active buffer has the right shebang
+            If so, set teh UDL
 
-selectUdlBasedOnShebang = SelectUdlBasedOnShebang()
-selectUdlBasedOnShebang.main()
+            Args:
+                shebangs:   dict mapping shebang to corresponding UDL name
+            Return:
+                Nothing
+        '''
+        firstLine = editor.getLine(0).rstrip();
+        if firstLine in shebangs:
+            found = shebangs[firstLine]
+            notepad.runMenuCommand('Language', found)
+
+
+if __name__ == '__main__':
+    selectUdlBasedOnShebang = SelectUdlBasedOnShebang()
 
 """
 Development Notes:
