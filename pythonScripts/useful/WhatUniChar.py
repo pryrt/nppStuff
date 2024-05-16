3ac0205544e297f1e44d981638a8d02009295180
pryrt <pryrtcode@pryrt.com>
2024-05-16 06:40:30 -0700

    add surrogate-pair listing for non-BMP Unicode characters

--- pythonScripts/useful/WhatUniChar.py
+++ pythonScripts/useful/WhatUniChar.py
@@ -19,7 +19,16 @@ def callback_sci_UPDATEUI(args):
 
     try:
         is_eof = (editor.getCurrentPos()==editor.getLength())
-        info = "'{1}' = HEX:0x{0:04X} = DEC:{0} ".format(c, s.encode('utf-8') if c not in [13, 10, 0] else 'LINE-ENDING' if c != 0 else 'END-OF_FILE' if is_eof else 'NUL')
+        if c < 0x10000:
+            info = "'{1}' = HEX:0x{0:04X} = DEC:{0} ".format(c, s.encode('utf-8') if c not in [13, 10, 0] else 'LINE-ENDING' if c != 0 else 'END-OF_FILE' if is_eof else 'NUL')
+        elif c < 0x110000:
+            # added the surrogate-pair listing for non-BMP Unicode
+            #   inspired by https://community.notepad-plus-plus.org/topic/25784/
+            L = 0xDC00 + (c & 0x3FF)
+            H = 0xD800 + (((c-0x10000)>>10)&0x3FF)
+            info = "'{1}' = HEX:0x{0:04X} = DEC:{0} ⇒ SURROGATE(0x{2:04X} 0x{3:04X})".format(c, s.encode('utf-8'), H, L)
+        else:
+            raise ValueError("Invalid Unicode character")
     except ValueError:
         info = "HEX:?? DEC:?"
     notepad.setStatusBar(STATUSBARSECTION.DOCTYPE, info)
@@ -30,6 +39,7 @@ callback_sci_UPDATEUI(None)     # per https://notepad-plus-plus.org/community/to
 '''
 unicode: ☐ ☑ ☒ ✓ ✗ ◯ ○ ⊙ ⦾ ⦿
 wide unicode: 🔘  (U+1F518)
+wide: 😞 (U+1F61E)
 
 inspired by https://notepad-plus-plus.org/community/topic/14767/ascii-hex-of-current-character
 but make a unicode version...
