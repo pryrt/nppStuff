5bb7b670b5d5e1e282c50e84c640b4c506cf9931
PeterCJ <pryrtcode@pryrt.com>
2024-08-03 09:46:25 -0700

    starting to experiment with Lexilla's SubStyle, per 25980

---
+++ pythonScripts/nppCommunity/25xxx/25980-SubStyle-Experiments.py
@@ -0,0 +1,92 @@
+# encoding=utf-8
+"""in response to https://community.notepad-plus-plus.org/topic/25980/
+
+Starting to experiment with SubStyles
+
+https://github.com/ScintillaOrg/lexilla/issues/260
+  - encouraged me to get the SciTE 5.5.1 source and prebuilt,
+    and after some prodding, able to get enough to see `decrypt` added as substyled keyword
+  - however, despite their assertion, I couldn't find examples in their code
+    of how they _use_ any of those commands -- not as SCI_xxx messages, nor as the
+    Scintilla::Message enumeration.
+  - but when I searched the source, was able to find their python-based testing framework
+
+<SciTE Source Directory>\scintilla\test\simpleTests.py :: TestSubStyles()
+  - shows how they _test_ SubStyles, which gives a pretty good example of how to use
+    at least some of the commands.  I will try it out, both on the CPP which they show in their
+    example, then try to replicate with
+"""
+from Npp import *
+import os
+import textwrap
+
+def tryCPP():
+    console.write("cpp lexer: \n")
+    tmpfile = os.path.join(os.environ["TEMP"], '25980_SubStyle.cpp')
+    if os.path.exists(tmpfile):
+        notepad.open(tmpfile)
+    else:
+        notepad.new()
+        notepad.saveAs(tmpfile)
+
+    editor.clearAll()
+    src = textwrap.dedent(r'''    #include <stdio.h>
+
+    int main()
+    {
+        printf("hello world\n");
+    }
+    ''')
+    editor.addText(src)
+    notepad.save()
+
+    bases = editor.getSubStyleBases()
+    bases_hex = ":".join("0x{0:02x}({0:>3d})".format(c) for c in bases.encode())
+    console.write(f"bases = \"{bases}\" = >>{bases_hex}<<\n")
+
+    #notepad.close()
+
+def tryPHP():
+    console.write("php lexer: \n")
+    tmpfile = os.path.join(os.environ["TEMP"], '25980_SubStyle.php')
+    if os.path.exists(tmpfile):
+        notepad.open(tmpfile)
+    else:
+        notepad.new()
+        notepad.saveAs(tmpfile)
+
+    editor.clearAll()
+    src = textwrap.dedent(r'''    <head> <!-- About to script -->
+    <?php
+    decrypt "xyzzy";
+    echo __FILE__.__LINE__;
+    echo "<!-- -->\n";
+    /* ?> */
+    ?>
+    <strong>for</strong><b>if</b>
+    <?= 'short echo tag' ?>
+    <? echo 'short tag' ?>
+    <script>
+        alert("<?php echo "PHP" . ' Code'; ?>");
+        alert('<?= 'PHP' . "Code"; ?>');
+        var xml =
+        '<?xml version="1.0" encoding="iso-8859-1"?><SO_GL>' +
+        '<GLOBAL_LIST mode="complete"><NAME>SO_SINGLE_MULTIPLE_COMMAND_BUILDER</NAME>' +
+        '<LIST_ELEMENT><CODE>1</CODE><LIST_VALUE><![CDATA[RM QI WEB BOOKING]]></LIST_VALUE></LIST_ELEMENT>' +
+        '<LIST_ELEMENT><CODE>1</CODE><LIST_VALUE><![CDATA[RM *PCC]]></LIST_VALUE></LIST_ELEMENT>' +
+        '</GLOBAL_LIST></SO_GL>';
+    </script>
+    ''')
+    editor.addText(src)
+    notepad.save()
+
+    bases = editor.getSubStyleBases()
+    bases_hex = ":".join("0x{0:02x}({0:>3d})".format(c) for c in bases.encode())
+    console.write(f"bases = \"{bases}\" = >>{bases_hex}<<\n")
+
+    #notepad.close()
+
+tryCPP()
+del(tryCPP)
+tryPHP()
+del(tryPHP)
