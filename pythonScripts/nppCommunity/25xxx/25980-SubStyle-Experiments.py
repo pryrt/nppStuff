73d3007a5a53b8ac344af119a5025cdc6c5cf503
PeterCJ <pryrtcode@pryrt.com>
2024-08-03 10:21:39 -0700

    found the FirstSubStyle for both LexCPP (0x80=128) and LexHTML (0xC0=192) - the latter makes sense for encrypt getting 198 as substyle

--- pythonScripts/nppCommunity/25xxx/25980-SubStyle-Experiments.py
+++ pythonScripts/nppCommunity/25xxx/25980-SubStyle-Experiments.py
@@ -50,8 +50,15 @@ def tryCPP():
     console.write(f"bases = \"{bases}\" = >>{bases_hex}<<\n")
     # bases = "" = >>0x0b( 11):0x11( 17)<<
     # ⇒ matches what the test say it should get: 11=C++ IDENTIFIER; 17=C++ COMMENT DOC KEYWORD
+    console.write("distance = 0x{0:02x}({0:>3d})\n".format(editor.distanceToSecondaryStyles()))
 
-    #notepad.close()
+    # LexCPP: SubStyles subStyles{ styleSubable, SubStylesFirst, SubStylesAvailable, inactiveFlag };
+    #   uses the global SubStyles.h `constexpr int SubStylesFirst = 0x80;` as its start-of-substyles region,
+    #   which is `firstSubStyle` in simpleTests::TestSubStyles::TestAllocate()
+    firstSubStyle = 0x80    # SubStyles.h::SubStylesFirst
+    console.write("GetStyleFromSubStyle = 0x{0:02x}({0:>3d})\n".format(editor.getStyleFromSubStyle(firstSubStyle)))
+
+    notepad.close()
 
 def tryPHP():
     console.write("php lexer: \n")
@@ -92,8 +99,15 @@ def tryPHP():
     console.write(f"bases = \"{bases}\" = >>{bases_hex}<<\n")
     # bases = ".=J`y" = >>0x01(  1):0x03(  3):0x2e( 46):0x3d( 61):0x4a( 74):0x60( 96):0x79(121)<<
     # ⇒ confirmed that styleID=121 (PHP KEYWORD) does allow substyles
+    console.write("distance = 0x{0:02x}({0:>3d})\n".format(editor.distanceToSecondaryStyles()))
 
-    #notepad.close()
+    # LexHTML: SubStyles subStyles{styleSubable,SubStylesHTML,SubStylesAvailable,0};
+    #   uses the local `constexpr int SubStylesHTML = 0xC0;` as its start-of-substyles region,
+    #   so I would want to use that here
+    firstSubStyle = 0xC0    # LexHTML.cxx::SubStylesHTML
+    console.write("GetStyleFromSubStyle = 0x{0:02x}({0:>3d})\n".format(editor.getStyleFromSubStyle(firstSubStyle)))
+
+    notepad.close()
 
 tryCPP()
 del(tryCPP)
