9f9da713838ac020a0294ff7dbdc56277e463a6f
PeterCJ <pryrtcode@pryrt.com>
2024-08-03 12:43:45 -0700

    use / instead of \ to avoid invalid-escape messages to console when running

--- pythonScripts/useful/copyUserScriptDirectory.py
+++ pythonScripts/useful/copyUserScriptDirectory.py
@@ -1,5 +1,5 @@
 # encoding=utf-8
-"""Puts the AppData-or-equivalent Plugins\Config\PythonScript\Scripts directory into the clipboard
+"""Puts the AppData-or-equivalent Plugins/Config/PythonScript/Scripts directory into the clipboard
 Run this right before calling Plugins > Python Script > New Script, then paste that directory to make sure you create the script in the right location
 """
 from Npp import editor,notepad
