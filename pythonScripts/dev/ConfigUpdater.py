d1b132165d57ba7be4299e8402b35fbc455c4889
pryrt <pryrtcode@pryrt.com>
2024-08-13 06:59:42 -0700

    finish addMissingLexer; preserve XML-comments; add ?xml declaration; update TODOs

--- pythonScripts/dev/ConfigUpdater.py
+++ pythonScripts/dev/ConfigUpdater.py
@@ -9,6 +9,13 @@ from Npp import editor,notepad,console
 import xml.etree.ElementTree as ET  # https://docs.python.org/3/library/xml.etree.elementtree.html
 import os
 
+class CommentedTreeBuilder(ET.TreeBuilder):
+    # https://stackoverflow.com/a/34324359/5508606
+    def comment(self, data):
+        self.start(ET.Comment, {})
+        self.data(data)
+        self.end(ET.Comment)
+
 class ConfigUpdater(object):
     def __init__(self):
         self.dirNpp          = notepad.getNppDir()
@@ -17,7 +24,9 @@ class ConfigUpdater(object):
 
     def go(self):
         self.update_stylers('stylers.xml')
-        # TODO: loop over all themes and call .update_stylers()
+        # TODO: loop over all themes (*) and call .update_stylers()
+        #   *: <dirNppConfig>\Themes\*
+        #   *: <dirNpp>\Themes\* -- but only if <dirNpp> != <dirNppConfig>
         self.update_langs()
 
     def update_stylers(self, themeName):
@@ -26,18 +35,17 @@ class ConfigUpdater(object):
             fTheme = os.path.join(self.dirNppConfig, themeName)
         else:
             fTheme = os.path.join(self.dirNppConfig, 'themes', themeName)
-        console.write("stylers.model = '{}'\ntheme = '{}'\n".format(fModel, fTheme))
+        #console.write("stylers.model = '{}'\ntheme = '{}'\n".format(fModel, fTheme))
 
-        # TODO: try to preserve comments by using
+        # preserve comments by using
         #   <https://stackoverflow.com/a/34324359/5508606>
-        #   see the comment about <?xml ... ?>, so I will need to use the options to turn those on, as well
 
-        treeModel = ET.parse(fModel)
-        treeTheme = ET.parse(fTheme)
+        treeModel = ET.parse(fModel, parser=ET.XMLParser(target=CommentedTreeBuilder()))
+        treeTheme = ET.parse(fTheme, parser=ET.XMLParser(target=CommentedTreeBuilder()))
 
         #https://github.com/pryrt/nppStuff/blob/cdd094148bd54f4b1c8e24cc328cc0afd558cf26/pythonScripts/nppCommunity/sessionChecker.py#L122
         # ... gives example of iterating through specific elements
-        # Better example is in officail docs, here:
+        # Better example is in official docs, here:
         #   https://docs.python.org/3/library/xml.etree.elementtree.html#finding-interesting-elements
         #   ROOT.iter('xxx') goes through all xxx nodes inside the ROOT (also works on whole TREE, not just ROOT)
         #       example = treeModel.iter('LexerType')   # iterates through all LexerType nodes
@@ -65,36 +73,48 @@ class ConfigUpdater(object):
             strToFind = ".//LexerType[@name='{}']".format(elModelLexer.attrib['name'])
             elStylersMatchLT = treeTheme.find(strToFind)
             if elStylersMatchLT is None:
-                #console.write("NOT FOUND[{}] => {}; TODO = update XML\n".format(strToFind, elStylersMatchLT))
+                #console.write("NOT FOUND[{}] => {}\n".format(strToFind, elStylersMatchLT))
                 self.addMissingLexer(elModelLexer, elThemeLexerStyles)
             else:
                 pass # console.write("YES FOUND[{}] => {}\n".format(strToFind, elStylersMatchLT.attrib))
 
-
-        # maybe: sort the lexers by name
+        # sort the lexers by name
         #       cf: https://stackoverflow.com/questions/25338817/sorting-xml-in-python-etree
         #       ``` def sortchildrenby(parent, attr):
         #       ```     parent[:] = sorted(parent, key=lambda child: child.get(attr))
         #       ``` sortchildrenby(root, 'NAME')
         #   use trick <https://stackoverflow.com/a/18411610/5508606> to get 'searchResult' sorted last
-        #elThemeLexerStyles[:] = sorted(elThemeLexerStyles, key=lambda child: (child.get('name') == 'searchResult', child.get('name')))
+        elThemeLexerStyles[:] = sorted(elThemeLexerStyles, key=lambda child: (child.get('name') == 'searchResult', child.get('name')))
+
+        # TODO: need to look for missing GlobalStyles elements as well
+        #   most important would be getting the right names and styleID,
+        #   but it would be nice to propagate the comments before "Global override" and before "Selected text colour" as well
 
         # fix the indentation for the whole tree
         ET.indent(treeTheme, space = "    ", level=0)
 
         # for now, show the result; TODO = write to file
-        console.write("Show full treeTheme:\n{}\n=====\n".format(ET.tostring(treeTheme.getroot(), encoding="unicode")))
-        console.write("\n\n")
+        #   use xml_declaration=True in order to get <?xml...?>
+        #   set encoding="unicode" in .tostring() to get printable string,
+        #       or encoding="UTF-8" in .tostring() or .write() to get the encoded bytes for writing UTF-8 to a file
+        console.write("{}\n".format(ET.tostring(treeTheme.getroot(), encoding="unicode", xml_declaration=True)))
 
     def addMissingLexer(self, elModelLexer, elLexerStyles):
-        console.write("addMissingLexer({})\n".format(elModelLexer.attrib['name']))
+        #console.write("addMissingLexer({})\n".format(elModelLexer.attrib['name']))
         elNewLexer = ET.SubElement(elLexerStyles, 'LexerType', attrib=elModelLexer.attrib)
-        attr = {} # TODO: inherit from self.globalStyle.attrib; but if I set it EQUAL, then attr[]=... will corrupt globalStyle
-        attr['fontName'] = ""
         for elWordsStyle in elModelLexer.iter("WordsStyle"):
-            console.write("- need WordsStyle => {}\n".format(elWordsStyle.attrib))
-            attr['name'] = elWordsStyle.attrib['name']
-            attr['styleID'] = elWordsStyle.attrib['styleID']
+            #console.write("- need WordsStyle => {}\n".format(elWordsStyle.attrib))
+            attr = {
+                'name': elWordsStyle.attrib['name'],
+                'styleID': elWordsStyle.attrib['styleID'],
+                'fgColor': elWordsStyle.attrib['fgColor'],
+                'bgColor': elWordsStyle.attrib['bgColor'],
+                'fontName': "",
+                'fontStyle': elWordsStyle.attrib['fontStyle'],
+                'fontSize': "",
+            }
+            if 'keywordClass' in elWordsStyle.attrib:
+                attr['keywordClass'] = elWordsStyle.attrib['keywordClass']
             ET.SubElement(elNewLexer, 'WordsStyle', attrib=attr)
 
         #ET.indent(elNewLexer, space = "    ", level=2)
