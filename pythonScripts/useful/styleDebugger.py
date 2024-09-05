46ef6488497e949c1150fbb11259b3f200909062
pryrt <pryrtcode@pryrt.com>
2024-09-05 08:10:41 -0700

    add a KEY of style/name/tags/desc using the lexer metadata

--- pythonScripts/useful/styleDebugger.py
+++ pythonScripts/useful/styleDebugger.py
@@ -15,13 +15,20 @@ class pyscStyleDebugger(object):
     def go(self):
         ps = None
         sout = ""
+        s_found = []
         for i in range(editor.getTextLength()):
             s = editor.getStyleIndexAt(i)
+            if s not in s_found:
+                s_found.append(s)
             if s != ps:
                 sout += "{{{}}}".format(s)
                 ps = s
             c = editor.getCharAt(i)
             sout += chr(c)
+
+        skey = ""
+        for s in sorted(s_found):
+            skey += "{{{:3}}}: {:<40} | {:<40} | {}\n".format(s, editor.nameOfStyle(s), editor.tagsOfStyle(s), editor.descriptionOfStyle(s))
 
         notepad.new()
         editor.setText(sout)
@@ -31,6 +38,9 @@ class pyscStyleDebugger(object):
         editor.styleSetFore(127, fg127)
         editor.styleSetBack(127, bg0)
         editor.research(r'{\d+}', self.onmatch)
+        editor.startStyling(editor.getTextLength(),0)
+        editor.appendText(skey)
+        editor.setStyling(len(skey), 127)
         editor.setSavePoint()
 
 
