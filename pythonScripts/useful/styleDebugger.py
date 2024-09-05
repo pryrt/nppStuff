1f38cf8bf03f71e2cc967c98cf2a5ea96bd223e6
pryrt <pryrtcode@pryrt.com>
2024-09-05 07:54:34 -0700

    this checks for all the style toggles in the active file, and outputs a {0}indicated{1} plaintext to show what styles are applied (useful for debugging styler problems)

---
+++ pythonScripts/useful/styleDebugger.py
@@ -0,0 +1,37 @@
+# encoding=utf-8
+#
+# tracks changes in style in the active file,
+# and creates a new plaintext document containing the original file, but indicating style changes with {##}
+#   so `{0}ABC{1}D{0}` means `ABC` are in styleID=0, and the `D` is in styleID=1
+
+from Npp import editor,notepad,console
+
+class pyscStyleDebugger(object):
+    def onmatch(self,m):
+        #console.write("style {} at {}\n".format(m.group(0), m.span(0)))
+        editor.startStyling(m.start(0), 0)
+        editor.setStyling(m.end(0)-m.start(0), 127)
+
+    def go(self):
+        ps = None
+        sout = ""
+        for i in range(editor.getTextLength()):
+            s = editor.getStyleIndexAt(i)
+            if s != ps:
+                sout += "{{{}}}".format(s)
+                ps = s
+            c = editor.getCharAt(i)
+            sout += chr(c)
+
+        notepad.new()
+        editor.setText(sout)
+        fg0 = editor.styleGetFore(0)
+        bg0 = editor.styleGetBack(0)
+        fg127 = tuple([int(fg0[x] + (bg0[x]-fg0[x]) * 0.75) for x in range(3)])
+        editor.styleSetFore(127, fg127)
+        editor.styleSetBack(127, bg0)
+        editor.research(r'{\d+}', self.onmatch)
+        editor.setSavePoint()
+
+
+pyscStyleDebugger().go()
