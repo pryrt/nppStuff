2d9f3ef07b026804f1adefae771fe1246c4172f9
pryrt <pryrtcode@pryrt.com>
2024-11-21 10:46:41 -0800

    HiddenLexers: X12 mostly working, though setProperty(fold,1) doesn't seem to do anything yet

--- pythonScripts/dev/HiddenLexers.py
+++ pythonScripts/dev/HiddenLexers.py
@@ -2,6 +2,11 @@
 '''
 Makes lexilla's "hidden" lexers available for NPP.
 
+The most recent version of this script is available at
+https://raw.githubusercontent.com/pryrt/nppStuff/refs/heads/main/pythonScripts/dev/HiddenLexers.py
+
+===========
+
 If you run it multiple times, it will toggle enabled/disabled.
 Note: when it disables, you have to switch out of that tab _on that view/editor#_ for it to stop highlighting
 
@@ -19,10 +24,14 @@ class GenericLexer:
     _lexer_name = b"Generic"
 
     def __init__(self):
-        self.lexer_name = create_unicode_buffer(self._lexer_name)
+        tmp_name = self._lexer_name
+        if sys.version_info.major==3: tmp_name = self._lexer_name.decode("utf-8")
+        self.lexer_name = create_unicode_buffer(tmp_name)
 
     def announce(self, lexintf):
-        # console.write("I will colorize {} from {}\n".format(self._lexer_name, str(lexintf)))
+        tmp_name = self._lexer_name
+        if sys.version_info.major==3: tmp_name = self._lexer_name.decode("utf-8")
+        console.write("I will colorize {} from {}\n".format(tmp_name, str(lexintf)))
         pass
 
     def colorize(self, lexintf):
@@ -182,6 +191,82 @@ class StataLexer(GenericLexer):
         editor.setKeyWords(0, "anova by ci clear correlate describe diagplot drop edit exit gen generate graph help if infile input list log lookup oneway pcorr plot predict qnorm regress replace save sebarr set sort stem summ summarize tab tabulate test ttest use")   # keywords SAS: %let %do
         editor.setKeyWords(1, "byte int long float double strL str") # types # SAS: also cards
 
+class X12Lexer(GenericLexer):
+    """
+        X12 EDI is a language for business-to-business transaction software
+
+        * https://x12.org/
+
+        * example: https://iwayinfocenter.informationbuilders.com/TLs/TL_soa_ebiz_edix12/source/sample_docs49.htm
+            ISA*00*          *01*          *ZZ*NOTP           *ZZ*NOTP           *050108*0954*U*00501*000000001*0*P*>
+            GS*IN*NOTP*NOTP*20050108*0954*1*X*004010
+            ST*810*0001
+            BIG*20021119*184*20021015*BMB
+            REF*IA*040682
+            N1*BT*WALGREEN*92*0000
+            ITD*02**1.000**30**31*****1% 30 NET 31
+            FOB*CC
+            PID*S**VI*FL
+            IT1*0001*267*CA*53.52**IN*859067
+            PID*F*08*VI**BARBIE SING W/ME DISC GRL CD PLYR
+            TDS*1421839*1428984
+            CAD*T***CFWY*CONSOLIDATED FREIGHTWAYS
+            SAC*A*D240***7145**********FREIGHT CHARGE
+            ISS*267*CA
+            CTT*1
+            SE*15*0001
+            GE*1*1
+            IEA*1*000000001
+
+        Looking at LexX12.cxx, does not appear to have any keyword lists.
+    """
+
+    _lexer_name = b"x12"
+    SCE_X12_DEFAULT                                    = 0
+    SCE_X12_BAD                                        = 1
+    SCE_X12_ENVELOPE                                   = 2
+    SCE_X12_FUNCTIONGROUP                              = 3
+    SCE_X12_TRANSACTIONSET                             = 4
+    SCE_X12_SEGMENTHEADER                              = 5
+    SCE_X12_SEGMENTEND                                 = 6
+    SCE_X12_SEP_ELEMENT                                = 7
+    SCE_X12_SEP_SUBELEMENT                             = 8
+
+    def colorize(self, lexintf):
+        self.announce(lexintf)
+
+        editor.styleSetFore(self.SCE_X12_DEFAULT                 , notepad.getEditorDefaultForegroundColor())
+        editor.styleSetFore(self.SCE_X12_BAD                     , (255,0,0))
+        editor.styleSetFore(self.SCE_X12_ENVELOPE                , (255,0,255))
+        editor.styleSetFore(self.SCE_X12_FUNCTIONGROUP           , (0,204,0))
+        editor.styleSetFore(self.SCE_X12_TRANSACTIONSET          , (0,0,204))
+        editor.styleSetFore(self.SCE_X12_SEGMENTHEADER           , (64,64,64))
+        editor.styleSetFore(self.SCE_X12_SEGMENTEND              , (128,128,128))
+        editor.styleSetFore(self.SCE_X12_SEP_ELEMENT             , (255,0,0))
+        editor.styleSetFore(self.SCE_X12_SEP_SUBELEMENT          , (255,127,255))
+
+        editor.styleSetBack(self.SCE_X12_BAD                     , (255,255,0))
+        editor.styleSetBack(self.SCE_X12_SEP_ELEMENT             , (237,237,237))
+        editor.styleSetBack(self.SCE_X12_SEP_SUBELEMENT          , (237,237,237))
+
+        # ordering is important
+        if lexintf.nppver() < 8.410:
+            self.ilexer_ptr = self.create_lexer_func(self.lexer_name.value)
+            #console.write("old: called create_lexer_func({})\n".format(self.lexer_name.value))
+        else:
+            self.ilexer_ptr = windll.user32.SendMessageW(lexintf.notepad_hwnd, lexintf.NPPM_CREATELEXER, 0, addressof(self.lexer_name))
+            #console.write("new: sendmessage NPPM_CREATELEXER({:s})\n".format(self.lexer_name.value))
+
+        editor_hwnd = lexintf.editor1_hwnd if notepad.getCurrentView() == 0 else lexintf.editor2_hwnd
+        windll.user32.SendMessageW(editor_hwnd, lexintf.SCI_SETILEXER, 0, self.ilexer_ptr)
+
+        # LexX12.cxx only defines one property; I want to enable folding
+        editor.setProperty("fold", 1)
+
+        # if I am wrong about keyword lists, each would go here...
+        #editor.setKeyWords(0, "word1 word2")
+        #editor.setKeyWords(1, "word11 word12")
+
 class HiddenLexerInterface:
     NPPM_CREATELEXER                                   = (1024 + 1000 + 110)
     SCI_SETILEXER                                      = 4033
@@ -205,6 +290,7 @@ class HiddenLexerInterface:
         self.map_extensions = {}
         self.map_extensions['do'] = self.map_extensions['stata'] = StataLexer()
         self.map_extensions['sas'] = SasLexer()
+        self.map_extensions['x12'] = X12Lexer()
 
         # initialize win32 interface info
         self.notepad_hwnd = windll.user32.FindWindowW(u'Notepad++', None)
@@ -343,9 +429,10 @@ class HiddenLexerInterface:
 try:
     lexer_interface.toggle()
     if True:
-        notepad.clearCallbacks()
+        notepad.clearCallbacks(lexer_interface.on_langchanged)
+        notepad.clearCallbacks(lexer_interface.on_bufferactivated)
         del lexer_interface
-        console.write("deleted callbacks and lexer_interface")
+        console.write("deleted callbacks and lexer_interface\n")
 except NameError:
     lexer_interface = HiddenLexerInterface()
     lexer_interface.main()
