d9c84066c89a94b3092a1e18f5359298fbad6e7a
PeterCJ <pryrtcode@pryrt.com>
2026-07-12 14:27:15 -0700

    customSessionSnapshot: get the POC working
    make it save the snapshot files
    
    define the TODOs, for cleaning out old snapshots, setup threading.Timer, and setup NOTIFICATIONS.SHUTDOWN==NPPM_BEFORESHUTDOWN

--- pythonScripts/nppCommunity/GitHubScripts/gh18198_customSessionSnapshot.py
+++ pythonScripts/nppCommunity/GitHubScripts/gh18198_customSessionSnapshot.py
@@ -1,17 +1,29 @@
 # encoding=utf-8
 """in response to https://github.com/notepad-plus-plus/notepad-plus-plus/issues/18198
 
-Experimenting with how I might solve such a problem
+Proof of concept of how I might solve such a problem
 
-Store all the globals at the very top, so they are easy for user to edit:
+Store all the user configuration globals at the very top, so they are easy for user to edit:
 ===== GLOBALS ====================================================================================
 """
-gsSnapshotDirectory = "c:/temp/"        # set this to your desired location; either use / or \\ as path separator
+gsSnapshotDirectory = "c:/temp/nppCustomSnapshot"        # set this to your desired location; either use / or \\ as path separator
 """
 ===== END GLOBALS ================================================================================
+
+===== TODO =======================================================================================
+[ ] figure out and clean out which existing snapshots aren't needed anymore
+    - my idea would be to recurse through all the files in gsSnapshotDirectory,
+        and any that aren't still in the dFilesToArchive should be cleaned out
+        (probably better to go in a seperate function call on the same modifiedFiles)
+[ ] setup time snapshot,
+    - threading.Timer() example: p27550_TimedDualSave.py
+    - every S seconds, it will call runCustomSessionSnapshot(), then schedule the next instance
+[ ] setup callback on NPPM_BEFORESHUTDOWN (NOTIFICATIONS.SHUTDOWN)
+    - call runCustomSessionSnapshot()
+    - cancel remaining threading.Timer instance
 """
 
-from Npp import notepad, editor, console
+from Npp import notepad, editor, console, BUFFERENCODING
 import pathlib
 import zlib
 
@@ -26,6 +38,7 @@ def runCustomSessionSnapshot():
         for tFileInfo in notepad.getFiles():
             sName = tFileInfo[0]
             pPath = pathlib.PureWindowsPath(sName)
+            sFileName = pPath.name
             pDir = pPath.parent
             sDirTail = pDir.name
             iCRC = zlib.crc32(str(pDir).encode('utf-8'))
@@ -40,7 +53,8 @@ def runCustomSessionSnapshot():
             kBufID = tFileInfo[1]
             if isModified and not kBufID in dTrackModifiedFiles:
                 dTrackModifiedFiles[kBufID] = {
-                    'name':         tFileInfo[0],
+                    'pathName':     tFileInfo[0],
+                    'fileName':     sFileName,
                     'index':        tFileInfo[2],
                     'view':         tFileInfo[3],
                     'archiveTail':  sArchiveTail
@@ -49,18 +63,47 @@ def runCustomSessionSnapshot():
         return dTrackModifiedFiles
 
     def archiveFiles(dFilesToArchive):
-        console.write(f"{str(dFilesToArchive):<120.120}\n")
+        encMap = {
+                # https://docs.python.org/3/library/codecs.html#text-encodings
+                int(BUFFERENCODING.COOKIE)       : 'utf-8',     # noBOM
+                int(BUFFERENCODING.ENC8BIT)      : 'mcbs',      # ANSI, CP_ACP
+                int(BUFFERENCODING.ANSI)         : 'ascii',     # badly named
+                int(BUFFERENCODING.UCS2BE)       : 'utf_16_be', # need manual BOM
+                int(BUFFERENCODING.UCS2BE_NOBOM) : 'utf_16_be',
+                int(BUFFERENCODING.UCS2LE)       : 'utf_16_le', # need manual BOM
+                int(BUFFERENCODING.UCS2LE_NOBOM) : 'utf_16_le',
+                int(BUFFERENCODING.UTF8)         : 'utf-8-sig'  # auto BOM
+        }
+        #console.write(f"{str(dFilesToArchive):<120.120}\n")
         for kBufID in dFilesToArchive.keys():
             d = dFilesToArchive[kBufID]
             notepad.activateIndex(d['view'], d['index'])
             e = notepad.getEncoding()
-            console.write(f"{str(True):<5.5} | {d['name']:<90.90} | {d['archiveTail']:<40.40} | v{d['view']}.i{d['index']} | e:{str(e):<12.12}\n")
+            t = editor.getText()
+            bytestring = t.encode(encMap[int(e)], errors='replace')
+            if e == BUFFERENCODING.UCS2BE:
+                bytestring = codec.BOM_UTF16_BE + bytestring
+            elif e == BUFFERENCODING.UCS2LE:
+                bytestring = codec.BOM_UTF16_LE + bytestring
+
+            pArchivePath = pathlib.PureWindowsPath(gsSnapshotDirectory).joinpath(d['archiveTail'], d['fileName'])
+
+            #console.write(f"{str(True):<5.5} | {d['pathName']:<90.90} | {str(pArchivePath):<90.90} | v{d['view']}.i{d['index']} | e:{encMap[int(e)]:<12.12} | l:{len(bytestring)}\n")
+
+            pathlib.WindowsPath(pArchivePath.parent).mkdir(parents=True, exist_ok=True)
+
+            with open(str(pArchivePath), 'wb') as f:
+                f.write(bytestring)
+
+        # TODO: I should also figure out which existing snapshots aren't needed anymore...
+        #   but that's more logic than I'm interested in for now.
+
 
     ############################################################################
     # Now that helper functions are defined, do the algorithm:
     ############################################################################
-    console.show()
-    console.clear()
+    #console.show()
+    #console.clear()
 
     # store orignal state
     tOriginalState = (notepad.getCurrentView(), notepad.getCurrentDocIndex(notepad.getCurrentView()), notepad.getCurrentDocIndex(0), notepad.getCurrentDocIndex(1))
